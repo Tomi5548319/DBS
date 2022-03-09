442c6b42dad29d992c54d39133a15ba09d7e94ca
Tomáš Oross <tomi.oross@gmail.com>
2022-03-09 22:43:20 +0100

    Revert "Uprava domovskej stranky"
    This reverts commit a8a3b4b9ffddf77915ab17c4cd6ee36a1f0811b1.

--- app.py
+++ app.py
@@ -6,23 +6,10 @@ import psycopg2 as psi
 import json
 from dotenv import dotenv_values
 
-
 @app.route('/')
 def index():
-   #print('Request for index page received')
-   return "Homepage" #render_template('index.html')
-
-
-"""
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       return render_template('hello.html', name = name)
-   else:
-       return redirect(url_for('index'))
-"""
+   print('Request for index page received')
+   return render_template('index.html')
 
 
 @app.route('/v1/health', methods=['GET'])
@@ -52,10 +39,17 @@ def dbs_je_best():
 
     return json.dumps(moj_dic)
 
-@app.route('/v2/patches', methods=['GET'])
-def dbs_je_best():
 
-    return "TODO"
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
