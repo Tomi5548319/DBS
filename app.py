a8a3b4b9ffddf77915ab17c4cd6ee36a1f0811b1
Tomáš Oross <tomi.oross@gmail.com>
2022-03-09 22:14:36 +0100

    Uprava domovskej stranky

--- app.py
+++ app.py
@@ -6,10 +6,23 @@ import psycopg2 as psi
 import json
 from dotenv import dotenv_values
 
+
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   #print('Request for index page received')
+   return "Homepage" #render_template('index.html')
+
+
+"""
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       return render_template('hello.html', name = name)
+   else:
+       return redirect(url_for('index'))
+"""
 
 
 @app.route('/v1/health', methods=['GET'])
@@ -39,17 +52,10 @@ def dbs_je_best():
 
     return json.dumps(moj_dic)
 
+@app.route('/v2/patches', methods=['GET'])
+def dbs_je_best():
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    return "TODO"
 
 
 if __name__ == '__main__':
