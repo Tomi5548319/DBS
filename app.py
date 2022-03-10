7b7b7ec2829634bf389db242ac1fbd549dcccb5f
Tomáš Oross <tomi.oross@gmail.com>
2022-03-10 14:12:53 +0100

    Test <path:text>

--- app.py
+++ app.py
@@ -56,44 +56,10 @@ def dbs_je_best():
     return json.dumps(moj_dic)
 
 
-@app.route('/v2/players/14944/game_exp', methods=['GET'])
-def game_xp():
-    auth = dotenv_values("/home/en_var.env")
-
-    conn = psi.connect(
-        host="147.175.150.216",
-        database="dota2",
-        user=auth["DBUSER"],
-        password=auth["DBPASS"])
-
-    kurzor = conn.cursor()
-    kurzor.execute(("SELECT pl.id,COALESCE(pl.nick, 'unknown') AS player_nick, "
-                    "hr.localized_name AS hero_localized_name, "
-                    "round(cast(mt.duration AS numeric)/60, 2) AS match_duration_minutes, "
-                    "COALESCE(ma.xp_hero, 0) + "
-                        "COALESCE(ma.xp_creep, 0) + "
-                        "COALESCE(ma.xp_roshan, 0)+ "
-                        "COALESCE(ma.xp_other, 0) as experiences_gained,"
-                    "ma.level as level_gained,"
-                    "CASE "
-                        "WHEN ma.player_slot>=0 AND ma.player_slot < 5 THEN mt.radiant_win "
-                        "WHEN ma.player_slot>=128 AND ma.player_slot < 133 THEN NOT mt.radiant_win "
-                    "END AS winner, "
-                    "ma.match_id FROM players AS pl "
-                 "JOIN matches_players_details AS ma ON pl.id=ma.player_id "
-                 "JOIN heroes AS hr ON ma.hero_id=hr.id "
-                 "JOIN matches AS mt ON mt.id=ma.match_id "
-                 "WHERE pl.id=2"))
-
-    return kurzor.fetchone()[0]
-
-
-"""@app.route('/v2/players/<string:id>/game_exp', methods=['GET'])
+@app.route('/v2/players/<path:text>', methods=['GET'])
 def game_xp():
 
-    return "Hrac " + id + " game_exp:"
-"""
-
+    return "Hrac " + path + " game_exp:"
 
 
 if __name__ == '__main__':
