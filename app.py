e3e5f5ccffb5fda8a85e5635889b899fd751481f
Tomáš Oross <tomi.oross@gmail.com>
2022-03-16 21:32:58 +0100

    Refaktoring

--- app.py
+++ app.py
@@ -9,6 +9,30 @@ import platform
 from dotenv import dotenv_values
 
 
+@app.route('/')
+def index():
+    return "<style>" \
+               "button {" \
+                    "background-color: #66B1FF;" \
+                    "border: none;" \
+                    "color: white;" \
+                    "padding: 15px 32px;" \
+                    "text-align: center;" \
+                    "text-decoration: none;" \
+                    "display: inline-block;" \
+                    "font-size: 16px;" \
+               "}" \
+            "</style>" \
+            "<script>" \
+                "function changeURL(extension) {" \
+                "window.location.href = window.location.href + extension" \
+           "}" \
+           "</script>" \
+           "<button onclick=\"changeURL('v1/health/');\">/v1/health/</button><br><br>" \
+           "<button onclick=\"changeURL('v2/patches/');\">/v2/patches/</button></form><br><br>" \
+           "<button onclick=\"changeURL('v2/players/14944/game_exp/');\">/v2/players/14944/game_exp/</button></form><br><br>"
+
+
 def get_linux_conn():
     auth = dotenv_values("/home/en_var.env")
 
@@ -33,49 +57,22 @@ def connect_to_database():
     return get_windows_conn()
 
 
-@app.route('/')
-def index():
-    return "<style>" \
-               "button {" \
-                    "background-color: #66B1FF;" \
-                    "border: none;" \
-                    "color: white;" \
-                    "padding: 15px 32px;" \
-                    "text-align: center;" \
-                    "text-decoration: none;" \
-                    "display: inline-block;" \
-                    "font-size: 16px;" \
-               "}" \
-            "</style>" \
-            "<script>" \
-                "function changeURL(extension) {" \
-                "window.location.href = window.location.href + extension" \
-           "}" \
-           "</script>" \
-           "<button onclick=\"changeURL('v1/health/');\">/v1/health/</button><br><br>" \
-           "<button onclick=\"changeURL('v2/patches/');\">/v2/patches/</button></form><br><br>" \
-           "<button onclick=\"changeURL('v2/players/14944/game_exp/');\">/v2/players/14944/game_exp/</button></form><br><br>"
-
-        #print('Request for index page received')
-    #"<button type=\"button\">Click Me!</button>" #render_template('index.html')
-
-
 @app.route('/v1/health/', methods=['GET'])
-def dbs_je_best():
+def v1_health():
     conn = connect_to_database()
 
     kurzor = conn.cursor()
     kurzor.execute("SELECT VERSION();")
-    response_version = kurzor.fetchone()
+    response_version = kurzor.fetchone()[0]
 
     kurzor.execute("SELECT pg_database_size('dota2')/1024/1024 as dota2_db_size;")
-    response_db_size = kurzor.fetchone()
+    response_db_size = kurzor.fetchone()[0]
 
     moj_dic = {}
     moj_vnoreny_dic = {}
 
-    moj_vnoreny_dic["version"] = response_version[0]
-    moj_vnoreny_dic["dota2_db_size"] = response_db_size[0]
+    moj_vnoreny_dic["version"] = response_version
+    moj_vnoreny_dic["dota2_db_size"] = response_db_size
 
     moj_dic['pgsql'] = moj_vnoreny_dic
 
@@ -85,7 +82,7 @@ def dbs_je_best():
     return json.dumps(moj_dic)
 
 
-@app.route('/v2/players/<string:player_id>/game_exp/', methods=['GET', 'POST'])
+@app.route('/v2/players/<string:player_id>/game_exp/', methods=['GET'])
 def v2_game_exp(player_id):
     conn = connect_to_database()
 
@@ -104,7 +101,7 @@ def v2_game_exp(player_id):
                    "vysledok.level_gained, "
                    "CASE WHEN side_played = 'radiant' AND vysledok.radiant_win = 'true' OR "
                    "side_played = 'dire' AND vysledok.radiant_win = 'false' "
-                   "THEN 'true' ELSE 'false' END AS winner "
+                   "THEN true ELSE false END AS winner "
                    "FROM ("
                    "SELECT players.id AS pid, COALESCE(nick, 'unknown') AS player_nick, heroes.localized_name AS h_name, "
                    "matches.id AS match_id, matches.duration, ROUND(matches.duration/60.0, 2) AS min, "
@@ -131,21 +128,16 @@ def v2_game_exp(player_id):
         match_dic['match_duration_minutes'] = float(row[2])
         match_dic['experiences_gained'] = row[3]
         match_dic['level_gained'] = row[4]
-        match_dic['winner'] = ("" + row[5]) in ['true', '1', 't', 'y', 'yes']
+        match_dic['winner'] = row[5]
 
         matches.append(match_dic)
 
     hlavny_dic['matches'] = matches
 
-
-    #response_version = kurzor.fetchone()
-
     kurzor.close()
     conn.close()
 
     return json.dumps(hlavny_dic)
-    #navrat  #"/v2/{player_id}/game_exp, {player_id} = " + player_id + "; <br>version: "
-           # + type(response_version) # Toto to crashne
 
 
 if __name__ == '__main__':
