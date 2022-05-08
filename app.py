0f63682d8aab12c6e2d698564186c32106030519
Tomáš Oross <tomi.oross@gmail.com>
2022-05-08 22:51:24 +0200

    Zmena verzie na azure

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 #metadata = Base.metadata
 
 #alchemy_env = dotenv_values("/home/en_var.env")
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + alchemy_env['DBUSER'] + ':' + alchemy_env['DBPASS'] + '@147.175.150.216/dota2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + os.getenv('DBUSER') + ':' + os.getenv('DBPASS') + '@147.175.150.216/dota2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + alchemy_env['DBUSER'] + ':' + alchemy_env['DBPASS'] + '@147.175.150.216/dota2'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + os.getenv('DBUSER') + ':' + os.getenv('DBPASS') + '@147.175.150.216/dota2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
